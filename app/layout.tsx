7ea47e40f581dedfed148850bfcefaa18145d4ed
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-26 23:10:56 +0800

    feat: few modifacations

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable, 'dark:bg-dark')}>{children}</body>
     </html>
   );
 }
