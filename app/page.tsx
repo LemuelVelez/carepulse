63623fe05cb21352b5207d61d3f357aa281bb9e0
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-29 14:14:02 +0800

    feat: side Image and copyright

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import PatientForm from "@/components/forms/PatientForm";
+import { Link } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,8 +16,24 @@ export default function Home() {
           />
 
           <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
