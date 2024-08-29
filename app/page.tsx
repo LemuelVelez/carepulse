7ab6b90d8602367c2832f28ce04a8711659a4002
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-29 14:32:08 +0800

    feat: form

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import PatientForm from "@/components/forms/PatientForm";
-import { Link } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP VERIFICATION | PasskeyModal */}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
