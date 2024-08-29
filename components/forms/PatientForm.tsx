72c0988d1d43c7b883e53d97e9cf8f49b8d8d492
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-29 14:39:03 +0800

    feat: section

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -34,7 +34,11 @@ const PatientForm = () => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section>
+          <h1 className="header">Hello there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
         <FormField
           control={form.control}
           name="username"
