3df1c76ed98ef64da27c24d8e7fca661711b2540
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-08-30 16:35:13 +0800

    feat: few changes

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -11,6 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
 import { FormFieldType } from "./forms/PatientForm";
+import Image from "next/image";
 
 interface CustomProps {
   control: Control<any>;
@@ -27,8 +28,34 @@ interface CustomProps {
   renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const RenderField = ({field, props}: {field: any, props: CustomProps}) => {
-  return <Input type="text" placeholder="John Doe" />;
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, iconSrc, iconAlt, placeholder } = props;
+
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              alt={iconAlt || "icon"}
+              width={24}
+              height={24}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      )
+    default:
+      break;
+  }
 };
 
 const CustomFormField = (props: CustomProps) => {
@@ -44,7 +71,7 @@ const CustomFormField = (props: CustomProps) => {
           )}
 
           <RenderField field={field} props={props} />
-          <FormMessage className="shad-error"/>
+          <FormMessage className="shad-error" />
         </FormItem>
       )}
     />
