22cab2d6d01db5ca4b83da0e0ed8291720b4458a
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-09-04 20:12:13 +0800

    feat: try catch error

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,1 +1,15 @@
-export const createUser = async (user: CreateUserParams) => {};
+import { Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+    } catch (error: any) {
+        if (error && error?.code === 409) {
+            const documents = await users.list([
+                Query.equal("email", [user.email])
+            ]);
+
+            return documents?.users[0];
+        }
+    }
+};
