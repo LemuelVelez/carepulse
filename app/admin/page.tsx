401e9bada41be45049f66c9dfa76ab017f610cec
Lemuel Tumala Velez <138591940+LemuelVelez@users.noreply.github.com>
2024-09-18 05:34:44 +0800

    feat: Image and Link

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={32}
+            width={162}
+            alt="Logo"
+            className="h-8 w-fit"
+          />
+        </Link>
+      </header>
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
